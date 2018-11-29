8edb96a6972001d12acb9900e0976006e7f03e67
Peter Hurst <30393194+phurst-gh@users.noreply.github.com>
2018-11-29 11:49:46 +0000

    added a clear/focus to script

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
+// I would prefer to make this more OOP but with the script only being small it felt
+// cleaner to do it through functions.
+
 function buildComment() {
-        let newComment, usersName, timePosted, postDraft;
+    let newComment, usersName, timePosted, postDraft;
 
-        //1. Gather Data
-        newComment = document.querySelector('#commInp').value;
-        usersName = 'John Doe'; //Display purposes
-        timePosted = 'Just now'; //Display purposes
-        console.log(newComment);
+    //1. Gather Data
+    newComment = document.querySelector('#commInp').value;
+    usersName = 'John Doe'; //Display purposes
+    timePosted = 'Just now'; //Display purposes
 
-        //2. Create template HTML
-        postDraft = '<div class="commWrap"><img class="avatarMed" src="comm_avatar.png" alt="User account avatar" /><div class="postInfo"><span class="userName textGreen textSemiBold">%namePlacehold%</span><span class="timePosted textRegularSml">%timePlacehold%</span><p class="userComm textRegularLrg">%commentPlacehold%</p><div class="commActionWrap"><div class="commAction commLike"><i class="fas fa-thumbs-up"></i>Like</div><div class="commAction commReply"><i class="fas fa-reply"></i>Reply</div></div></div></div>';
+    //2. Create template HTML
+    postDraft = '<div class="commWrap"><img class="avatarMed" src="comm_avatar.png" alt="User account avatar" /><div class="postInfo"><span class="userName textGreen textSemiBold">%namePlacehold%</span><span class="timePosted textRegularSml">%timePlacehold%</span><p class="userComm textRegularLrg">%commentPlacehold%</p><div class="commActionWrap"><div class="commAction commLike"><i class="fas fa-thumbs-up"></i>Like</div><div class="commAction commReply"><i class="fas fa-reply"></i>Reply</div></div></div></div>';
 
-        //3. Update template HTML with poster's data
-        postFinal = postDraft.replace('%commentPlacehold%', newComment);
-        postFinal = postFinal.replace('%namePlacehold%', usersName);
-        postFinal = postFinal.replace('%timePlacehold%', timePosted);
+    //3. Update template HTML with poster's data
+    postFinal = postDraft.replace('%commentPlacehold%', newComment);
+    postFinal = postFinal.replace('%namePlacehold%', usersName);
+    postFinal = postFinal.replace('%timePlacehold%', timePosted);
 
-        //4. Update UI with new post
-        document.querySelector('.commWrapAll').insertAdjacentHTML('beforeend', postFinal);
+    //4. Update UI with new post
+    document.querySelector('.commWrapAll').insertAdjacentHTML('beforeend', postFinal);
+
+    //5. Clear Input and set focus
+    clearPostInp()
 };
+
+function clearPostInp() {
+    document.querySelector('.commInp').value = '';
+    document.querySelector('.commInp').focus();
+}
 
 //Event Listeners
 document.querySelector('.postCommBtn').addEventListener('click', buildComment);
